9a61ba1eb8c4559cdcf429787a8dbd88f810566d
Silvio Endruhn <silvio@endruhn.de>
2026-03-10 20:01:35 +0100

    add menü im Backend

--- mobile-browser-bar-color.php
+++ mobile-browser-bar-color.php
@@ -16,15 +16,40 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function mbbc_init() {
     add_action( 'wp_head', 'mbbc_add_meta_tags' );
+    add_action( 'customize_register', 'mbbc_customize_register' );
 }
 add_action( 'init', 'mbbc_init' );
+
+/**
+ * Registriert die Customizer-Einstellungen unter Design > Customizer.
+ */
+function mbbc_customize_register( $wp_customize ) {
+    // Sektion
+    $wp_customize->add_section( 'mbbc_section', array(
+        'title'    => __( 'Mobile Browser-Bar Farbe', 'mbbc' ),
+        'priority' => 160,
+    ) );
+
+    // Einstellung
+    $wp_customize->add_setting( 'mbbc_color', array(
+        'default'           => '#000000',
+        'sanitize_callback' => 'sanitize_hex_color',
+        'transport'         => 'postMessage',
+    ) );
+
+    // Colorpicker-Control
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'mbbc_color', array(
+        'label'   => __( 'Browser-Bar Farbe (HEX)', 'mbbc' ),
+        'section' => 'mbbc_section',
+    ) ) );
+}
 
 /**
  * Fügt die Meta-Tags für die mobile Browser-Bar-Farbe hinzu.
  */
 function mbbc_add_meta_tags() {
-    // Standardfarbe für die Browser-Bar (Schwarz)
-    $color = apply_filters( 'mbbc_browser_bar_color', '#000' ); // Ermöglicht Anpassung durch andere Entwickler.
+    $color = get_theme_mod( 'mbbc_color', '#000000' );
+    $color = apply_filters( 'mbbc_browser_bar_color', $color );
     ?>
     <!-- Meta-Tags für die mobile Browser-Bar-Farbe -->
     <meta name="theme-color" content="<?php echo esc_attr( $color ); ?>">
